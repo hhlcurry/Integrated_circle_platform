0a736e09876f3ffb8dff2bb45d3bf353ad87360a
hhl <2633528624@qq.com>
2023-10-07 14:49:16 +0800

    首次提交

---
+++ Integrated_circle_platform/src/main/java/com/niit/integrated_circle_platform/IntegratedCirclePlatformApplication.java
@@ -0,0 +1,15 @@
+package com.niit.integrated_circle_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratedCirclePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegratedCirclePlatformApplication.class, args);
+        System.out.println("jjjj");
+    }
+
+
+}
---
+++ Integrated_circle_platform/src/test/java/com/niit/integrated_circle_platform/IntegratedCirclePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.integrated_circle_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratedCirclePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
